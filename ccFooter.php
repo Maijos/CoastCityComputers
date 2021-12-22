e33f058c9e8cb062a5a0a5fca0edf06f6ff9bb1a
Joe Maiello <86252649+Maijos@users.noreply.github.com>
2021-12-22 10:49:35 -0500

    Add files via upload

---
+++ ccFooter.php
@@ -0,0 +1,9 @@
+<!-- Exercise 2-5 pages 119- 121 -->
+<!-- This is the footer component of cc computers project -->
+<hr/>
+<table width="100%" style="border: 0;">
+	<tr>
+		<td><strong>Updated;</strong>&nbsp;&nbsp;December 20, 2021</td>
+		<td style="text-align: right;">&copy;2021 by Coast City Computers</td>
+	</tr>
+</table>
---
+++ ccHeader.php
@@ -0,0 +1,10 @@
+<!-- Exercise 2-5 pages 119- 121 -->
+<!-- This is the header component of cc computers project -->
+<table width="100%" style="border: 0;">
+	<tr>
+		<td><h1 style="color: red; font-family: 'Georgia';">Coast City Computers</h1></td>
+		<td style="text-align: right;"><strong>Buy online or call 1-877-393-4448</strong></td>
+	</tr>	
+
+</table>
+<hr/>
